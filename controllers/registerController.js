85296d0f94290ff8f40d213111faeae84e53814e
Tais <calcagno.luizfelipe@gmail.com>
2025-07-23 12:26:57 -0300

    Update registerController.js

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import { createClient } from '@supabase/supabase-js';
-import sendVerificationEmail from '../utils/sendVerificationEmail.js';
+import sendVerificationEmail from '../utils/verificationEmail.js';
 import crypto from 'crypto';
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);

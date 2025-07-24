2f0c87f47eca2a98837fef689774589f5665a2f2
Tais <calcagno.luizfelipe@gmail.com>
2025-07-24 11:34:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
-import registerRouter from './routes/register.js';
+import registerRouter from './routes/auth/register.js';
 
 dotenv.config();
 
@@ -16,7 +16,7 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/routes/auth/register.js', registerRouter);
+app.use('./routes/auth/register.js', registerRouter);
 
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');

e7245f76bd831ae6001dac49673cf88b1b19ced8
Tais <calcagno.luizfelipe@gmail.com>
2025-07-25 10:49:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
+app.options('*', cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
